17f19c474e4d5f6c347714295bbaf7af084153e4
babblebey <babblebey@gmail.com>
2024-03-23 22:41:14 +0100

    feat(build): add/config `mdx`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });

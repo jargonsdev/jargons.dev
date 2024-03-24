9de7e6fe27f3e78a4f81ffa21cd38285d060405b
babblebey <babblebey@gmail.com>
2024-03-24 12:28:06 +0100

    feat(build): add `react` integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react()]
 });

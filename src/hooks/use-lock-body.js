6ca9629a633644014eb584adf361199a17340abf
babblebey <babblebey@gmail.com>
2024-03-24 14:58:18 +0100

    feat: add `useLockBody` hook for island usage

---
+++ src/hooks/use-lock-body.js
@@ -0,0 +1,11 @@
+import { useLayoutEffect } from "react";
+
+const useLockBody = () => {
+  useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = originalStyle);
+  }, []);
+};
+
+export default useLockBody;

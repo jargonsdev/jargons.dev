32ef563fac82ef6b9d2478de5529465bfe1b9b2e
babblebey <babblebey@gmail.com>
2024-03-25 11:50:01 +0100

    feat: add `$recentSearches` and `$addToRecentSearchesFn` to `search` store

--- src/stores/search.js
+++ src/stores/search.js
@@ -1,3 +1,35 @@
-import { atom } from "nanostores";
+import { atom, map } from "nanostores";
 
 export const $isSearchOpen = atom(false);
+
+/**
+ * @typedef {Object} SearchedItem
+ * @property {string} word
+ * @property {string} url
+ */
+
+/** @type {import('nanostores').MapStore<Record<string, SearchedItem>>} */
+export const $recentSearches = map({});
+
+/**
+ * Add search term to recent search history
+ * @param {SearchedItem} item 
+ */
+export function $addToRecentSearchesFn({ word, url }) {
+  const lowercaseKey = word.toLowerCase();
+  const key = lowercaseKey.includes(" ") ? lowercaseKey.split(" ").join("-") : lowercaseKey;
+  const isInRecentSearch = $recentSearches.get()[key];
+
+  if (!isInRecentSearch) {
+    const recentSearchesCopy = $recentSearches.get();
+    recentSearchesCopy[key] = {
+      word, 
+      url 
+    };
+    $recentSearches.set({...recentSearchesCopy});
+  } else {
+    return;
+  }
+  
+  localStorage.setItem("devJargonsRecentSearches", JSON.stringify($recentSearches.get()));
+}

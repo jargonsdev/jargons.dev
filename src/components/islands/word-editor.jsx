6b72cc2f5dd2be8edc123d5034cbcf4b2ed49c57
babblebey <babblebey@gmail.com>
2024-03-27 05:00:14 +0100

    feat: implement `Editor` block in `word-editor` island

--- src/components/islands/word-editor.jsx
+++ src/components/islands/word-editor.jsx
@@ -1,15 +1,38 @@
+import { useEffect } from "react";
+import useDictionaryEditor from "../../hooks/use-dictionary-editor.js";
+
 export default function WordEditor({ title, content }) {
   return (
-    <div>
-      <Editor />
+    <div className="w-full flex border rounded-lg">
+      <Editor 
+        eTitle={"Hello"} 
+        eContent={"# Markdown \n content"} 
+        className="w-full h-full flex flex-col p-5 border-r"
+      />
       <Preview />
     </div>
   );
 }
 
-function Editor() {
+function Editor({ eTitle, eContent, className, ...props }) {
+  const { title, setTitle, content, setContent } = useDictionaryEditor();
+  
+  useEffect(() => {
+    setTitle(eTitle);
+    setContent(eContent);
+  }, []);
+
   return (
-    <></>
+    <div 
+      className={`${className}`}
+      {...props}
+    >
+      <textarea 
+        className="w-full grow resize-none appearance-none border-none focus:outline-none scrollbar"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+    </div>
   );
 }
 

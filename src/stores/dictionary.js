fb10691539a8489d26ef438e610f4a7c903a2e22
babblebey <babblebey@gmail.com>
2024-03-27 04:46:32 +0100

    feat: add `$dictionaryEditor` value to `dictionary` store

--- src/stores/dictionary.js
+++ src/stores/dictionary.js
@@ -1,3 +1,15 @@
-import { atom } from "nanostores";
+import { atom, map } from "nanostores";
 
 export const $dictionary = atom([]);
+
+/**
+ * @typedef {Object} DictionaryItem
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {import('nanostores').MapStore<Record<string, DictionaryItem>>} */
+export const $dictionaryEditor = map({
+  title: "",
+  content: ""
+});

70f147934b9aa38ced2a53e965c2b43d5f7b98ae
babblebey <babblebey@gmail.com>
2024-03-27 04:50:47 +0100

    feat: boilerplate `word-editor` island

---
+++ src/components/islands/word-editor.jsx
@@ -0,0 +1,20 @@
+export default function WordEditor({ title, content }) {
+  return (
+    <div>
+      <Editor />
+      <Preview />
+    </div>
+  );
+}
+
+function Editor() {
+  return (
+    <></>
+  );
+}
+
+function Preview() {
+  return (
+    <></>
+  );
+}

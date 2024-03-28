38084599cc41da2d60d8656fb3f6a2f089255bf2
babblebey <babblebey@gmail.com>
2024-03-28 23:32:01 +0100

    feat(build): add/configure `@astro/node` integration for `SSR`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });

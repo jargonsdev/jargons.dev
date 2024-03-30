27eaf8a4028f5fec6048b3f356bfb13a41e16408
babblebey <babblebey@gmail.com>
2024-03-30 20:14:19 +0100

    refactor: move `getRepoParts` util function to `src/lib/utils`; remove `lib/utils`

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -7,3 +7,18 @@ export function resolveCookieExpiryDate(expireIn) {
   const now = new Date();
   return new Date(now.getTime() + expireIn * 1000);
 }
+
+/**
+ * Get repository Owner and Name from fullname
+ * @param {string} repoFullName
+ * @returns an object with `repoOwner` and `repoName`
+ */
+export function getRepoParts(repoFullName) {
+  const parts = repoFullname.split("/");
+  const [ repoOwner, repoName ] = [ parts[0], parts[1] ];
+
+  return {
+    repoOwner,
+    repoName
+  }
+}

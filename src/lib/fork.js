700fd08b0df5d9278064a6c5af30daf6d28cef0c
Olabode Lawal-Shittabey <babblebey@gmail.com>
2024-03-31 19:15:29 +0100

    feat: implement `fork` repository script (#3)
    This Pull Request implements the `fork` script; this script is intended
    to be used to programmatically fork the main project repo to a user
    account; It houses a main function and other helper functions it uses to
    perform some necessary actions in order to ensure an efficient repo fork
    operation.
    
    ### Changes Made 
    
    - Implemented the main `forkRepository` function within script; this
    function is the main exported function that performs the main fork
    operation; it accepts a `userOctokit` (a user authenticated object with
    permission to act on behalf of the user) instance and the project's
    repository details i.e. `repoDetails` object and it does the
    following...
    - It checks whether the project repository has already been forked to
    the user's account using the `isRepositoryForked` helper function; this
    returns the `fork` of `null`
    - If the repo has already been forked, then we perform a check whether
    the fork is up-to-date/in sync with main project repo using the
    `isRepositoryForkUpdated` helper function; this returns the `updatedSHA`
    and a boolean `isUpdated` property that confirm whether fork is
    up-to-date
    - If fork is not up-to-date; then we perform the update by bringing it
    in sync with the main project repo using the `updateRepositoryFork`
    helper function
    - If repo is up-to-date/in sync with main project repo; we cancel out of
    the operation at this point with an early return;
    - If the project repository is not forked onto the user's account; then
    we proceed to initiating a fork process by calling the `"POST
    /repos/{owner}/{repo}/forks"` endpoint using the `userOctokit` instance.
    **_(This starts the fork process, we do not know exactly when the
    process completes 🤔)_**
    - Implement the following helper functions consumed within the main
    `forkRepository` function and within other helper functions too
    - `updateRepositoryFork` - used to update (Sync) repository to state of
    main (head) repository
    - `isRepositoryForkUpdated` - used to check whether a fork is (in Sync
    with head repo) up-to-date with main repo
      - `getBranch` - used to fetch a Branch/Ref details
    - `isRepositoryForked` - used to check for the presence of a specific
    repo in a user's fork repo list
    - Added `getRepoParts` to `/lib/utils`; its a utility function that is
    used to resolve `repoOwner` and `repoName` from a repository fullname.
    
    ### Related Issue
    
    Resolves #2
    
    ### Screencast/Screenshot
    
    
    https://github.com/babblebey/jargons.dev/assets/25631971/16221b7e-3c28-4c6c-a1f3-24d583ce7e3a
    
    📖

---
+++ src/lib/fork.js
@@ -0,0 +1,155 @@
+import { getRepoParts } from "./utils/index.js";
+
+/**
+ * Fork the project (specified) Repo to user account
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {{ repoFullname: string, repoMainBranchRef: string }} repo 
+ */
+export async function forkRepository(userOctokit, repoDetails) {
+  const { repoFullname, repoMainBranchRef } = repoDetails;
+  const { repoOwner, repoName } = getRepoParts(repoFullname);
+
+  try {
+    const { data: user } = await userOctokit.request("GET /user");
+
+    const fork = await isRepositoryForked(userOctokit, repoFullname, user.login ); 
+
+    if (!!fork) {
+      console.log("Repo is already forked!");
+
+      const { isUpdated, updateSHA } = await isRepositoryForkUpdated(userOctokit, repoDetails, fork)
+
+      if (!isUpdated) {
+        console.log("Repo is outdated!");
+
+        await updateRepositoryFork(userOctokit, fork, { 
+          ref: repoMainBranchRef, 
+          sha: updateSHA 
+        });
+      }
+
+      return;
+    }
+
+    const response = await userOctokit.request("POST /repos/{owner}/{repo}/forks", {
+      owner: repoOwner,
+      repo: repoName,
+    });
+
+    if (response.status === 202) {
+      console.log("Forking process initiated successfully!");
+    } else {
+      console.log("Error occurred while forking repository.");
+    }
+  } catch (error) {
+    console.log("Error occurred while forking repository:", error);
+  }
+}
+
+/**
+ * Update (Sync) repository to state of main (head) repository
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {string} fork 
+ * @param {{ ref: string, sha: string }} headRepoRef 
+ */
+async function updateRepositoryFork(userOctokit, fork, headRepoRef) {
+  const { repoOwner, repoName } = getRepoParts(fork);
+  const { ref, sha } = headRepoRef;
+
+  try {
+    await userOctokit.request("PATCH /repos/{owner}/{repo}/git/refs/{ref}", {
+      owner: repoOwner,
+      repo: repoName,
+      ref, //-> `heads/${branchToSync}`
+      sha
+    });
+
+    console.log("Fork is now updated and in-sync with upstream");
+  } catch (error) {
+    console.error("Error syncing with upstream:", error.message);
+    throw error;
+  }
+}
+
+/**
+ * Check whether a fork is (in Sync with head repo) up-to-date with main repo  
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {{ repoFullname: string, repoMainBranchRef: string }} repoDetails 
+ * @param {string} fork 
+ * @returns {{ isUpdated: boolean, updateSHA: string }}
+ */
+async function isRepositoryForkUpdated(userOctokit, repoDetails, fork) {
+  const { repoFullname, repoMainBranchRef } = repoDetails;
+
+  const userForkedBranch = await getBranch(userOctokit, fork, repoMainBranchRef);
+  const projectBranch = await getBranch(userOctokit, repoFullname, repoMainBranchRef);
+
+  return {
+    isUpdated: userForkedBranch.object.sha === projectBranch.object.sha,
+    updateSHA: projectBranch.object.sha
+  };
+}
+
+/**
+ * Get a Branch/Ref details
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {string} repo 
+ * @param {string} ref 
+ * @returns Branch/Ref details
+ */
+async function getBranch(userOctokit, repo, ref) {
+  const { repoOwner, repoName } = getRepoParts(repo);
+
+  const response = await userOctokit.request("GET /repos/{owner}/{repo}/git/ref/{ref}", {
+    owner: repoOwner,
+    repo: repoName,
+    ref: ref,
+  });
+
+  return response.data;
+}
+
+/**
+ * Check for the presence of a specific repo in a user's fork repo list
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {string} repoFullname 
+ * @param {string} userLogin 
+ * @returns { string | null }
+ * 
+ * @todo paginate response to get a list of all forks in one call
+ */
+async function isRepositoryForked(userOctokit, repoFullname, userLogin) {
+  try {
+    const response = await userOctokit.graphql(`#graphql
+      query forks($login: String!) {
+        user (login: $login) { 
+          repositories(first: 100, isFork: true) {
+            nodes {
+              name
+              owner {
+                login
+              }
+              parent {
+                name
+                owner {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+    `, { 
+      login: userLogin 
+    });
+
+    const { repoOwner, repoName } = getRepoParts(repoFullname);
+    
+    const matchingFork = response.user.repositories.nodes.find((fork) => fork.parent && fork.parent.owner.login === repoOwner && fork.parent.name === repoName) 
+      ?? null;
+    
+    return matchingFork ? matchingFork.owner.login + "/" + matchingFork.name : null;
+  } catch (error) {
+    console.log("Error occurred while checking repo fork: ", error);
+  }
+}
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -7,3 +7,18 @@ export function resolveCookieExpiryDate(expireIn) {
   const now = new Date();
   return new Date(now.getTime() + expireIn * 1000);
 }
+
+/**
+ * Get repository Owner and Name from fullname
+ * @param {string} repoFullname
+ * @returns an object with `repoOwner` and `repoName`
+ */
+export function getRepoParts(repoFullname) {
+  const parts = repoFullname.split("/");
+  const [ repoOwner, repoName ] = [ parts[0], parts[1] ];
+
+  return {
+    repoOwner,
+    repoName
+  }
+}

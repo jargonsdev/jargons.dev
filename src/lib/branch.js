c94b690eed146085b59e3f7a56cdcbc429d9dd7a
babblebey <babblebey@gmail.com>
2024-04-01 10:47:18 +0100

    doc: improve doc for `createBranch`  function

--- src/lib/branch.js
+++ src/lib/branch.js
@@ -1,12 +1,15 @@
 import { getRepoParts } from "./utils/index.js";
 
 /**
- * 
+ * Create a new branch on a given repository
  * @param {import("octokit").Octokit} userOctokit 
  * @param {{ repoFullname: string, repoMainBranchRef: string }} repoDetails 
- * @param {string} branchName 
+ * @param {string} newBranchName 
+ * @returns New Branch/Ref details
+ * 
+ * @todo handle error for `'Reference already exists'`
  */
-export async function createBranch(userOctokit, repoDetails, branchName) {
+export async function createBranch(userOctokit, repoDetails, newBranchName) {
   const { repoFullname, repoMainBranchRef } = repoDetails;
   const { repoName, repoOwner } = getRepoParts(repoFullname);
   const { object: { sha } } = await getBranch(userOctokit, repoFullname, repoMainBranchRef);
@@ -14,7 +17,7 @@ export async function createBranch(userOctokit, repoDetails, branchName) {
   const response = await userOctokit.request("POST /repos/{owner}/{repo}/git/refs", {
     owner: repoOwner,
     repo: repoName,
-    ref: `refs/heads/${branchName}`,
+    ref: `refs/heads/${newBranchName}`,
     sha,
   });
 

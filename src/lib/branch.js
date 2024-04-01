07f6f9da336229361bc058d2a67548fa8365d850
Olabode Lawal-Shittabey <babblebey@gmail.com>
2024-04-01 14:11:37 +0100

    feat: implement `branch` creation script (#17)
    This Pull request implements the branch creation script whose primary
    function is to create a new branch from the forked repository for an
    authenticated user.
    
    ### Changes Made 
    
    - Implemented the main `createBranch` function in the `branch` script at
    `src/lib/branch.js`; this function takes in 3 params;
    - `userOctokit` - a user authenticated instance of octokit that can be
    used to perform action on user's behalf
    - `repoDetails` - the user's forked repo details; hold the
    `repoFullname` and `repoMainBranchRef` as properties in the object type
    param
    - `newBranchName` - name of the branch that will be created for the user
      
    The `createBranch` function performs the following operation in the
    stated order...
    - It initially fetches the forked repository main branch where we wished
    to create the new branch off of; it does this using the `getBranch`
    helper function; this return the `SHA` of the branch
    - Then we make a request to the endpoint `"POST
    /repos/{owner}/{repo}/git/refs"` to create the new branch; passing in
    the `repoDetails.repoFullname` properties, the head branch `SHA` and the
    `newBranchName`.
    
    - Moved the `getBranch` function from the `fork` script to the `branch`
    script
    
    ### Screencast/Screenshot
    
    
    [screencast-bpconcjcammlapcogcnnelfmaeghhagj-2024.04.01-13_54_14.webm](https://github.com/babblebey/jargons.dev/assets/25631971/62a2f9b1-7643-42e3-b91f-1427fad1c170)

---
+++ src/lib/branch.js
@@ -0,0 +1,50 @@
+import { getRepoParts } from "./utils/index.js";
+
+/**
+ * Create a new branch on a given repository
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {{ repoFullname: string, repoMainBranchRef: string }} repoDetails 
+ * @param {string} newBranchName 
+ * @returns New Branch/Ref details
+ * 
+ * @todo handle error for `'Reference already exists'`
+ */
+export async function createBranch(userOctokit, repoDetails, newBranchName) {
+  const { repoFullname, repoMainBranchRef } = repoDetails;
+  const { repoName, repoOwner } = getRepoParts(repoFullname);
+  const { object: { sha } } = await getBranch(userOctokit, repoFullname, repoMainBranchRef);
+
+  try {
+    const response = await userOctokit.request("POST /repos/{owner}/{repo}/git/refs", {
+      owner: repoOwner,
+      repo: repoName,
+      ref: `refs/heads/${newBranchName}`,
+      sha,
+    });
+  
+    return response.data
+  } catch (error) {
+    throw new Error("error creating a new branch", { 
+      cause: error.message 
+    })
+  }
+}
+
+/**
+ * Get a Branch/Ref details
+ * @param {import("octokit").Octokit} userOctokit 
+ * @param {string} repo 
+ * @param {string} ref 
+ * @returns Branch/Ref details
+ */
+export async function getBranch(userOctokit, repo, ref) {
+  const { repoOwner, repoName } = getRepoParts(repo);
+
+  const response = await userOctokit.request("GET /repos/{owner}/{repo}/git/ref/{ref}", {
+    owner: repoOwner,
+    repo: repoName,
+    ref: ref,
+  });
+
+  return response.data;
+}

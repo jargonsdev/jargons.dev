be6bfe79da85d899eead5487dfd3f368394fef89
babblebey <babblebey@gmail.com>
2024-04-03 13:48:56 +0100

    feat: implement base function in `submitWord` function

--- src/lib/submit-word.js
+++ src/lib/submit-word.js
@@ -1,11 +1,29 @@
+import { getRepoParts } from "./utils/index.js";
+
 /**
  * 
- * @param {*} userOctokit 
+ * @param {import("octokit").Octokit} userOctokit 
  * @param {"new" | "edit"} action 
  * @param {{ repoFullname: string, repoBranchRef: string }} mainRepoDetails 
- * @param {{ repoFullname: string, repoBranchRef: string }} forkedRepoDetails 
+ * @param {{ repoFullname: string, repoChangeBranchRef: string }} forkedRepoDetails 
  * @param {{ title: string, content: string }} word 
  */
-function submitWord(userOctokit, action, repoDetails, forkedRepoDetails, word) {
+export async function submitWord(userOctokit, action, mainRepoDetails, forkedRepoDetails, word) {
+  const { repoFullname, repoBranchRef: mainRepoBranchRef } = mainRepoDetails;
+  const { repoName, repoOwner } = getRepoParts(repoFullname);
+  const { repoOwner: forkedRepoOwner } = getRepoParts(forkedRepoDetails.repoFullname);
+  const baseBranch = mainRepoBranchRef.split("/").slice(2).join("/");
+  const headBranch = forkedRepoDetails.repoChangeBranchRef.split("/").slice(2).join("/");
 
+  const response = await userOctokit.request("POST /repos/{owner}/{repo}/pulls", {
+    owner: "babblebey",
+    repo: "test",
+    title: "title test",
+    body: "body content \n text",
+    head: "sbabblebey:word/new/tuple", //`${headBranch}`, //forkedRepoDetails.repoChangeBranchRef.split("/").slice(2).join("/"),
+    // head_repo: "sbabblebey/test-mm",
+    base: "main"
+  });
+
+  return response.data;
 }

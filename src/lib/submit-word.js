0fcfe7afba96a5549bee21b273cd2d8813700cbc
babblebey <babblebey@gmail.com>
2024-04-03 07:15:38 +0100

    feat: boilerplate `submitWord` function in `submit-word` script

---
+++ src/lib/submit-word.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {*} userOctokit 
+ * @param {"new" | "edit"} action 
+ * @param {{ repoFullname: string, repoBranchRef: string }} mainRepoDetails 
+ * @param {{ repoFullname: string, repoBranchRef: string }} forkedRepoDetails 
+ * @param {{ title: string, content: string }} word 
+ */
+function submitWord(userOctokit, action, repoDetails, forkedRepoDetails, word) {
+
+}

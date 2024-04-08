565c31b99d46352616315bbdb6dec610a66d77a6
babblebey <babblebey@gmail.com>
2024-04-09 00:13:49 +0100

    doc(steps): document step in `handleSubmitWord` handler

--- src/lib/handlers/handle-submit-word.js
+++ src/lib/handlers/handle-submit-word.js
@@ -1,3 +1,16 @@
-export default function handleSubmitWord(e) {
-  console.log("This ws clicked!!!");
+import { forkRepository } from "../fork.js";
+import { createBranch } from "../branch.js";
+import { submitWord } from "../submit-word.js";
+import { updateExistingWord } from "../word-editor.js";
+
+/**
+ * Submit Handler for word editor, handles submit function based on action
+ * @param {"new" | "edit"} action 
+ * @param {{ title: string, content: string }} word 
+ */
+export default function handleSubmitWord(devJargonsOctokit, userOctokit, action, { title, content }) {
+  // Fork repo
+  // Create a branch for action
+  // update existing word
+  // submit the edit in new pr
 }

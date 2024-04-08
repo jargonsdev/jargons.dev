1f93b2f9a999628450d78e39766d91784cebc77c
babblebey <babblebey@gmail.com>
2024-04-08 17:36:23 +0100

    feat: add `edit-word-pr` template

---
+++ src/lib/templates/edit-word-pr.md.js
@@ -0,0 +1,11 @@
+const title = `Dictionary (Edit Word): $word_title`;
+
+const content = `### Word
+
+#### $word_title
+
+### Updated Meaning/Definition
+
+$word_content`;
+
+export default { title, content };

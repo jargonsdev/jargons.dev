7320ade487cec7091bc2220051e9fd26c53d50b3
babblebey <babblebey@gmail.com>
2024-04-08 03:11:53 +0100

    feat: implement new word pr template in `submit-word` script

--- src/lib/submit-word.js
+++ src/lib/submit-word.js
@@ -1,18 +1,19 @@
 import { getRepoParts } from "./utils/index.js";
+import newWordPRTemp from "./templates/new-word-pr.md.js";
 
 /**
  * 
  * @param {import("octokit").Octokit} userOctokit 
  * @param {"new" | "edit"} action 
- * @param {{ repoFullname: string, repoBranchRef: string }} mainRepoDetails 
+ * @param {{ repoFullname: string, repoMainBranchRef: string }} projectRepoDetails 
  * @param {{ repoFullname: string, repoChangeBranchRef: string }} forkedRepoDetails 
  * @param {{ title: string, content: string }} word 
  */
-export async function submitWord(userOctokit, action, mainRepoDetails, forkedRepoDetails, word) {
-  const { repoFullname, repoBranchRef: mainRepoBranchRef } = mainRepoDetails;
+export async function submitWord(userOctokit, action, projectRepoDetails, forkedRepoDetails, word) {
+  const { repoFullname, repoMainBranchRef } = projectRepoDetails;
   const { repoName, repoOwner } = getRepoParts(repoFullname);
   const { repoOwner: forkedRepoOwner } = getRepoParts(forkedRepoDetails.repoFullname);
-  const baseBranch = mainRepoBranchRef.split("/").slice(2).join("/");
+  const baseBranch = repoMainBranchRef.split("/").slice(2).join("/");
   const headBranch = forkedRepoDetails.repoChangeBranchRef.split("/").slice(2).join("/");
 
   const response = await userOctokit.request("POST /repos/{owner}/{repo}/pulls", {
@@ -20,8 +21,8 @@ export async function submitWord(userOctokit, action, mainRepoDetails, forkedRep
     repo: repoName,
     head: `${forkedRepoOwner}:${headBranch}`,
     base: baseBranch,
-    title: "title test", // word.title
-    body: "body content \ntext", // word.content
+    title: newWordPRTemp.title.replace("$word_title", word.title),
+    body: newWordPRTemp.content.replace("$word_title", word.title).replace("$word_content", word.content)
   });
 
   return response.data;

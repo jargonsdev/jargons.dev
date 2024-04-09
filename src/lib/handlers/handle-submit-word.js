458bf7da7a2b12e6ef7d28e77fa85db51fa626ba
babblebey <babblebey@gmail.com>
2024-04-09 21:55:00 +0100

    feat: modify params in `handleSubmitWord` handler

--- src/lib/handlers/handle-submit-word.js
+++ src/lib/handlers/handle-submit-word.js
@@ -1,3 +1,4 @@
+import { Octokit } from "@octokit-next/core";
 import { forkRepository } from "../fork.js";
 import { createBranch } from "../branch.js";
 import { submitWord } from "../submit-word.js";
@@ -5,10 +6,14 @@ import { updateExistingWord } from "../word-editor.js";
 
 /**
  * Submit Handler for word editor, handles submit function based on action
+ * @param {{user: string, devJargons: string}} octokitAuths 
  * @param {"new" | "edit"} action 
  * @param {{ title: string, content: string }} word 
  */
-export default function handleSubmitWord(devJargonsOctokit, userOctokit, action, { title, content }) {
+export default async function handleSubmitWord(octokitAuths, action, { title, content }) {
+  const userOctokit = new Octokit({ auth: octokitAuths.user });
+  const devJargonsOctokit = new Octokit({ auth: octokitAuths.devJargons });
+
   // Fork repo
   // Create a branch for action
   // update existing word

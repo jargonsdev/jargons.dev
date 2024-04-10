393f62aaa40c7d7c8496e96e96e994776ceaa7d8
babblebey <babblebey@gmail.com>
2024-04-10 03:23:51 +0100

    feat: implement word edit functionality in `handleSubmitWord` handler

--- src/lib/handlers/handle-submit-word.js
+++ src/lib/handlers/handle-submit-word.js
@@ -2,6 +2,7 @@ import { Octokit } from "@octokit-next/core";
 import { forkRepository } from "../fork.js";
 import { createBranch } from "../branch.js";
 import { submitWord } from "../submit-word.js";
+import { normalizeAsUrl } from "../utils/index.js";
 import { updateExistingWord } from "../word-editor.js";
 
 /**
@@ -9,16 +10,69 @@ import { updateExistingWord } from "../word-editor.js";
  * @param {{user: string, devJargons: string}} octokitAuths 
  * @param {"new" | "edit"} action 
  * @param {{ repoFullname: string, repoMainBranchRef: string }} projectRepoDetails 
- * @param {{ title: string, content: string }} word 
+ * @param {{ title: string, content: string, metadata: object }} word 
  * 
  * @todo would be nice to have `projectRepoDetails` work directly from `constants` or `env`
  */
-export default async function handleSubmitWord(octokitAuths, action, projectRepoDetails, { title, content }) {
+export default async function handleSubmitWord(octokitAuths, action, projectRepoDetails, { title, content, metadata }) {
   const userOctokit = new Octokit({ auth: octokitAuths.user });
   const devJargonsOctokit = new Octokit({ auth: octokitAuths.devJargons });
 
+  console.log(octokitAuths);
+  console.log(metadata);
+
   // Fork repo
+  const fork = await forkRepository(userOctokit, projectRepoDetails);
+  console.log("Project Fork: ", fork);
+
   // Create a branch for action
+  const branch = await createBranch(
+    userOctokit, 
+    {
+      repoFullname: fork,
+      repoMainBranchRef: projectRepoDetails.repoMainBranchRef
+    },
+    generateBranchName(action, title)
+  );
+  console.log("Branch Created: ", branch);
+
+  const forkedRepoDetails = {
+    repoFullname: fork,
+    repoChangeBranchRef: branch.ref
+  }
+  
   // update existing word
+  const word = await updateExistingWord(userOctokit, forkedRepoDetails, {
+    title,
+    content,
+    path: metadata.path,
+    sha: metadata.sha
+  }, {
+    env: "browser"
+  });
+  console.log("Word updated: ", word);
+
   // submit the edit in new pr
+  const wordSubmission = await submitWord(
+    devJargonsOctokit, 
+    userOctokit, 
+    action, 
+    projectRepoDetails, 
+    forkedRepoDetails, 
+    {
+      title, 
+      content
+    }
+  );
+  console.log("Word submitted: ", wordSubmission);
+}
+
+/**
+ * Generate branch name
+ * @param {string} action 
+ * @param {string} wordTitle 
+ * @returns {string}
+ */
+function generateBranchName(action, wordTitle) {
+  return `word/${action}/${normalizeAsUrl(wordTitle)}`;
 }

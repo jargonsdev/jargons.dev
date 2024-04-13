e8af293f43d9d66ac2ad3c809b9f3c26cd8db21e
babblebey <babblebey@gmail.com>
2024-04-13 10:19:07 +0100

    refactor: modify `PROJECT_REPO_DETAILS` constants property value

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 export const PROJECT_REPO_DETAILS = {
-  repoFullname: import.meta.env.PROJECT_REPO,
-  repoMainBranchRef: import.meta.env.PROJECT_REPO_BRANCH_REF
+  repoFullname: import.meta.env.PUBLIC_PROJECT_REPO,
+  repoMainBranchRef: import.meta.env.PUBLIC_PROJECT_REPO_BRANCH_REF
 }

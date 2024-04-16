d3c738c45077e4772ba463a614308e2cb5a9c880
Olabode Lawal-Shittabey <babblebey@gmail.com>
2024-04-16 21:46:38 +0100

    feat: implement jargons editor (dashboard) (#41)
    This Pull request implement the Jargons Editor (dashboard); This
    dashboard currently welcomes a user, presents them with a link to adding
    new word to dictionary and displays the current user's contributions
    stats (currently stats for contributions made via the jargons editor)
    with insights into their "total words contributed", "pending word
    contribution", "new word type contribution", and "edited word type
    contribution"; it does this by performing a search of pull request
    authored by the current user using a mix labels and pr states.
    
    ### Changes Made
    
    - Implemented and integrated the `doContributionStats` action on
    `/editor` page; this action runs on server-side before the `/editor`
    renders; it fetches all the contribution stats of the current user and
    returns the insights as required; it uses the GitHub Search API mixing a
    combination of the stated labels and search qualifiers in the search
    queries to get stated stats...
    - **Pending Word Contribution Stats** - it uses the following search
    qualifiers `repo:[our_repo] is:pull-request type:pr author:@me
    label:":computer: via word-editor is:unmerged is:open"`, which searches
    for opened pull requests on our repo stated repository that is authored
    by current user, with the labels `💻 via word-editor`
    - **New Word Type Contribution Stats** - it uses the following search
    qualifiers `repo:[our_repo] is:pull-request type:pr author:@me
    label:":computer: via word-editor label:":book: new word" is:merged
    is:closed"`, which searches for merged pull requests on our repo stated
    repository that is authored by current user, with the labels `💻 via
    word-editor` and `📖 new word`
    - **Edited Word Type Contribution Stats** - it uses the following search
    qualifiers `repo:[our_repo] is:pull-request type:pr author:@me
    label:":computer: via word-editor label:":book: edit word" is:merged
    is:closed"`, which searches for merged pull requests on our repo stated
    repository that is authored by current user, with the labels `💻 via
    word-editor` and `📖 edit word`
    - Implemented the `buildStatsUrl` to compute url to searching the
    particular stats on the main repo using the respective qualifiers; this
    means on click the built url a user is directed to github pull request
    search on stated repo with search result displayed specific to stats
    (powered by search filter)
    - Implemented 3 sections on the `/editor` i.e. the Jargon Editor
    (Dashboard)
    - User Profile Section - Display a welcome message with the current
    user's name and avatar with data retrieved from authentication data
    - Contribution CTA Section - Displays a static message and a link to
    `/editor/new` page to add new word to dictionary
    - Contribution Stats Section - Displays the users contribution stats;
    with an extra "Word Contributed" stats field with value manually
    calculated from the summation of "New Word Type Contribution Stats" and
    "Edited Word Type Contribution Stats"
    - Made Some Changes that are not so related
    - Implemented customization to `Navbar` component back navigation link
    using props with default value
        - Fixed overflowing height of the `WordEditor` component island
    - Added `maintainers_can_modify` to pr creation request in `submit-word`
    script
    
    ### Screencast/Screenshot
    
    
    ![image](https://github.com/babblebey/jargons.dev/assets/25631971/64477aa1-0c53-4a85-9273-8bc4f0ed58a0)
    
    
    [screencast-bpconcjcammlapcogcnnelfmaeghhagj-2024.04.16-21_30_22.webm](https://github.com/babblebey/jargons.dev/assets/25631971/dda9875f-3c43-436b-a3b2-220af938cc30)

---
+++ src/lib/actions/do-contribution-stats.js
@@ -0,0 +1,60 @@
+import app from "../octokit/app.js";
+import { decrypt } from "../utils/crypto.js";
+import { PROJECT_REPO_DETAILS } from "../../../constants.js";
+
+/**
+ * Get some jargons contribution stats for current user on the Jargons Editor
+ * @param {import("astro").AstroGlobal} astroGlobal 
+ */
+export default async function doContributionStats(astroGlobal) {
+  const { cookies } = astroGlobal;
+  const { repoFullname, repoMainBranchRef } = PROJECT_REPO_DETAILS; 
+
+  const accessToken = cookies.get("jargons.dev:token", { decode: value => decrypt(value) });
+  const userOctokit = app.getUserOctokit({ token: accessToken.value });
+
+  /**
+   * @todo Implement narrowed search to project's main branch 
+   */
+  const baseQuery = `repo:${repoFullname} is:pull-request type:pr author:@me label:":computer: via word-editor"`;
+  const baseStatsUrlQuery = `is:pr author:@me label:":computer: via word-editor"`;
+
+  /**
+   * @todo [thoughts]: would be nice to have all these requests in one, and use a filter to separate by labels
+   * [potential bottleneck]: no wat to know whether a PR is "merged" considering that "closed" doesn't mean merged
+   */
+  const { data: newType } = await userOctokit.request("GET /search/issues", {
+    q: `${baseQuery} label:":book: new word" is:merged is:closed`
+  });
+  const { data: editType } = await userOctokit.request("GET /search/issues", {
+    q: `${baseQuery} label:":book: edit word" is:merged is:closed`
+  });
+  const { data: pendingType } = await userOctokit.request("GET /search/issues", {
+    q: `${baseQuery} is:unmerged is:open`
+  });
+
+  return {
+    newWords: {
+      count: newType.total_count,
+      url: buildStatsUrl(repoFullname, `${baseStatsUrlQuery} is:merged is:closed label:":book: new word"`)
+    },
+    editedWords: {
+      count: editType.total_count,
+      url: buildStatsUrl(repoFullname, `${baseStatsUrlQuery} is:merged is:closed label:":book: edit word"`)
+    },
+    pendingWords: {
+      count: pendingType.total_count,
+      url: buildStatsUrl(repoFullname, `${baseStatsUrlQuery} is:unmerged is:open`)
+    }
+  }
+}
+
+/**
+ * Build URL to the Pull Request list on the project's Repo
+ * @param {string} repoFullname 
+ * @param {string} queryString 
+ * @returns {string}
+ */
+function buildStatsUrl(repoFullname, queryString) {
+  return `https://github.com/${repoFullname}/pulls?q=${encodeURIComponent(queryString)}`;
+}

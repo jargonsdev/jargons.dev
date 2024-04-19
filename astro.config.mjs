74f14f1c013c3788a5505d49d18d7f3939ef54e3
babblebey <babblebey@gmail.com>
2024-04-19 19:23:38 +0100

    fix: vercel error try 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    includeFiles: ["./src/pages/open-graph/_IBMPlexMono-SemiBold.ttf"]
+  })
 });

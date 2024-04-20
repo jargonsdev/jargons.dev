8149795f512f026e6f088224d29f09a30f404b59
babblebey <babblebey@gmail.com>
2024-04-20 12:17:15 +0100

    feat: implement `description` meta modifying `ogDescription`; implement `keywords`; add new constants `SITE_META_DESCRIPTION` and `SITE_META_KEYWORDS`

--- constants.js
+++ constants.js
@@ -2,3 +2,18 @@ export const PROJECT_REPO_DETAILS = {
   repoFullname: import.meta.env.PUBLIC_PROJECT_REPO,
   repoMainBranchRef: import.meta.env.PUBLIC_PROJECT_REPO_BRANCH_REF
 }
+
+export const SITE_META_DESCRIPTION = "A community-driven dictionary that simplifies software, engineering and tech terms for all levels. Curated by contributors, jargons.dev offers clear, easy-to-understand definitions."
+
+export const SITE_META_KEYWORDS = [  
+  "dev jargon",  
+  "dev jargons",  
+  "software engineering terms",  
+  "web dev terms",  
+  "software jargons",  
+  "software terms",  
+  "developer jargons",  
+  "web jargons",  
+  "web development terms",  
+  "software development jargons"
+];

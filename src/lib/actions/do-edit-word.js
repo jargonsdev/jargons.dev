4b95196c26eefb4d8b3872325f507c32d0ee9f3d
ShyneADL <benex2002@gmail.com>
2024-11-19 20:28:59 +0100

    fixed login issues by changing the cookie name to appropriate naming convention

--- src/lib/actions/do-edit-word.js
+++ src/lib/actions/do-edit-word.js
@@ -4,15 +4,24 @@ import { PROJECT_REPO_DETAILS } from "../../../constants.js";
 
 /**
  * Edit Word action - meant to be executed on `editor/edit/[word]` route
- * @param {import("astro").AstroGlobal} astroGlobal 
+ * @param {import("astro").AstroGlobal} astroGlobal
  */
 export default async function doEditWord(astroGlobal) {
-  const { cookies, params: { word } } = astroGlobal;
+  const {
+    cookies,
+    params: { word },
+  } = astroGlobal;
 
-  const accessToken = cookies.get("jargons.dev:token", { decode: value => decrypt(value) });
+  const accessToken = cookies.get("jargonsdevToken", {
+    decode: (value) => decrypt(value),
+  });
   const userOctokit = app.getUserOctokit({ token: accessToken.value });
 
-  const response = await getExistingWord(userOctokit, PROJECT_REPO_DETAILS, word);
+  const response = await getExistingWord(
+    userOctokit,
+    PROJECT_REPO_DETAILS,
+    word
+  );
 
   return response;
 }

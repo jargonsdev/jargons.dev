5fe923c1894789b10f8c36e412b15beab784012b
OJ Abba <phavorabba@gmail.com>
2024-11-22 04:15:04 +0000

    🚨 Fix recent searches console error

--- src/components/islands/recent-searches.jsx
+++ src/components/islands/recent-searches.jsx
@@ -4,29 +4,33 @@ import { $recentSearches } from "../../lib/stores/search.js";
 
 /**
  * Recent Searches Component - An Island that displays a user's last 5 searches
- * 
- * @todo implement a default list instead of `null` when no `$recentSearch` is found 
+ *
+ * @todo implement a default list instead of `null` when no `$recentSearch` is found
  * @todo implement loading component to avoid flickering UI
  */
 export default function RecentSearches() {
-  const recentSearches = useStore($recentSearches);
+	const recentSearches = useStore($recentSearches);
 
-  useEffect(() => {
-    $recentSearches.set({...JSON.parse(localStorage.getItem("jargons.dev:recent_searches"))})
-  }, []);
+	useEffect(() => {
+		$recentSearches.set({
+			...JSON.parse(localStorage.getItem("jargons.dev:recent_searches")),
+		});
+	}, []);
 
-  return Object.values(recentSearches).length ? (
-    <div className="space-y-3 ml-2 mt-4 md:mt-6">
+	return Object.values(recentSearches).length ? (
+		<div className="space-y-3 ml-2 mt-4 md:mt-6">
 			<h2 className="text-2xl md:text-4xl font-black">Recent</h2>
 			<ol className="space-y-1.5 underline">
-        {Object.values(recentSearches).slice(0, 5).map((item, i) => (
-          <li key={i}>
-            <a href={item.url}>
-              { item.word }
-            </a>
-          </li>
-        ))}
+				{Object.values(recentSearches)
+					.slice(0, 5)
+					.map((item, i) => (
+						<li key={i}>
+							<a href={item.url}>{item.word}</a>
+						</li>
+					))}
 			</ol>
 		</div>
-  ) : null;
+	) : (
+		<></>
+	);
 }

374c519bb37d9fb11c48b6dbb4f567cbb8a23c0b
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2024-11-22 20:56:58 +0000

    fix: invalid hook call and analytic measurement_id error (#114)
    ### Description
    This PR will:
    - Fix the `MEASUREMENT_ID` variable not being recognised in the base
    layout (It was logging an error in the browser).
    - Fix the "Invalid hook call" issue that occurred when starting the dev
    server.
    
    
    ### Related Issue
    Fixes #23 
    
    
    
    ### Screenshots/Screencasts
    N/A
    
    
    
    ### Notes to Reviewer
    - There's a slight formatting change to the affected file due to to my
    eslint config (I tried my best to configure it to match the source).
    - Slightly unrelated to this PR, but I feel it would be better to have
    the recent word in a row(not sure what to call it exactly) layout (Very
    rough sketch below).
    
    
    
    ![IMG_1997](https://github.com/user-attachments/assets/b75cf945-1ac4-4e3f-82b8-4c94b3c401df)
    
    ---------
    
    Co-authored-by: babblebey <babblebey@gmail.com>

--- src/components/islands/recent-searches.jsx
+++ src/components/islands/recent-searches.jsx
@@ -4,8 +4,8 @@ import { $recentSearches } from "../../lib/stores/search.js";
 
 /**
  * Recent Searches Component - An Island that displays a user's last 5 searches
- * 
- * @todo implement a default list instead of `null` when no `$recentSearch` is found 
+ *
+ * @todo implement a default list instead of `null` when no `$recentSearch` is found
  * @todo implement loading component to avoid flickering UI
  */
 export default function RecentSearches() {
@@ -17,8 +17,8 @@ export default function RecentSearches() {
 
   return Object.values(recentSearches).length ? (
     <div className="space-y-3 ml-2 mt-4 md:mt-6">
-			<h2 className="text-2xl md:text-4xl font-black">Recent</h2>
-			<ol className="space-y-1.5 underline">
+	  <h2 className="text-2xl md:text-4xl font-black">Recent</h2>
+	  <ol className="space-y-1.5 underline">
         {Object.values(recentSearches).slice(0, 5).map((item, i) => (
           <li key={i}>
             <a href={item.url}>
@@ -26,7 +26,9 @@ export default function RecentSearches() {
             </a>
           </li>
         ))}
-			</ol>
-		</div>
-  ) : null;
+	  </ol>
+	</div>
+  ) : (
+	<></>
+  );
 }

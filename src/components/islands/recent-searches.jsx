3d07c7638d6c1fd05bfb876f23d2836f11c3d2d1
Ayendi <mrayendi1@gmail.com>
2024-11-24 05:43:15 +0100

    removed .slice() method

--- src/components/islands/recent-searches.jsx
+++ src/components/islands/recent-searches.jsx
@@ -19,7 +19,7 @@ export default function RecentSearches() {
     <div className="space-y-3 ml-2 mt-4 md:mt-6">
 			<h2 className="text-2xl md:text-4xl font-black">Recent</h2>
 			<ol className="space-y-1.5 underline">
-        {Object.values(recentSearches).slice(0, 5).map((item, i) => (
+        {Object.values(recentSearches).map((item, i) => (
           <li key={i}>
             <a href={item.url}>
               { item.word }

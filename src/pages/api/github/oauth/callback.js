b1f4896be9421324407c5c5fe9339ecdec108305
babblebey <babblebey@gmail.com>
2025-06-24 13:47:03 +0100

    fix: ensure proper redirection and reload on successful authentication in login page

--- src/pages/api/github/oauth/callback.js
+++ src/pages/api/github/oauth/callback.js
@@ -12,8 +12,12 @@ export async function GET({ url: { searchParams }, redirect }) {
 
   const path = state.includes("path") && state.split("|")[0].split(":")[1];
 
-  if (path) return redirect(`${path}?code=${code}`);
+  /**
+   * Making sure a statically rendered page is not redirected to;
+   * ...they can't handle server-side operations like using authorization code
+   */
+  if (path && !path.includes("jai=1")) return redirect(`${path}?code=${code}`);
 
   // Lifeline/Last resort for when the return `path` is NOT specified/found in state
-  return redirect(`/login?return_to=${encodeURIComponent("/")}`)
+  return redirect(`/login?return_to=${encodeURIComponent(path || "/")}&code=${code}`);
 }

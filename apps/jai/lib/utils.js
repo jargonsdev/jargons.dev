32d83e67d30b7a31de7854716fc95393f81322dc
babblebey <babblebey@gmail.com>
2025-07-06 01:08:08 +0100

    feat(apps): add core functionality for `jAI` assistant with model, prompt, and utility exports

---
+++ apps/jai/lib/utils.js
@@ -0,0 +1,10 @@
+import type { Message } from "ai";
+
+/**
+ * Formats a message into a string
+ * @param {Message} message The message to format
+ * @returns The formatted message
+ */
+export const formatMessage = (message) => {
+  return `${message.role}: ${message.content}`;
+};
---
+++ apps/jai/lib/vector-store.js
@@ -0,0 +1,20 @@
+/**
+ * @todo Nice to have: Setup vectorStore for local development and production, and use based on the environment
+ * @todo ...use `MemoryVectorStore` for local development and `QdrantVectorStore` for production
+ */
+
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+
+// Initialize the OpenAI embeddings
+const embeddings = new OpenAIEmbeddings({
+  model: process.env.OPENAI_EMBEDDINGS_MODEL,
+});
+
+// Load vector store collection
+const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  url: process.env.QDRANT_URL,
+  collectionName: "dictionary",
+});
+
+export { vectorStore as default };

7a61bac3b4a242d889fc0ed87dacfe82699c0993
babblebey <babblebey@gmail.com>
2025-07-14 16:48:16 +0100

    Revert "fix(config): update vercel import to use serverless version"
    This reverts commit 59026c3f0ba628ad4d35230eb0efc6cf8d0bc8cd.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 

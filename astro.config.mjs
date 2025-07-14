59026c3f0ba628ad4d35230eb0efc6cf8d0bc8cd
babblebey <babblebey@gmail.com>
2025-07-14 16:43:40 +0100

    fix(config): update vercel import to use serverless version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 

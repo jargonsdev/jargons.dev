037c20a4ab59fa8e76953d3194cf8bdaede16301
babblebey <babblebey@gmail.com>
2025-09-22 20:56:18 +0100

    fix: add error handling in JAIWordSearch component

--- src/components/islands/jai-word-search.jsx
+++ src/components/islands/jai-word-search.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import { useChat } from "@ai-sdk/react";
 
 export default function JAIWordSearch({ word }) {
+  const [error, setError] = useState(null);
+
   /**
    * Initialize useChat hook
    */
   const { messages, status, append } = useChat({
     api: "/api/jai/search",
     onError: (e) => {
+      setError(JSON.parse(e.message));
       console.error(e);
     },
   });
@@ -33,6 +36,35 @@ export default function JAIWordSearch({ word }) {
         <div className="h-6 w-3/4 animate-pulse bg-gray-200 rounded-md" />
         <div className="h-6 w-4/5 animate-pulse bg-gray-200 rounded-md" />
         <div className="h-6 w-3/5 animate-pulse bg-gray-200 rounded-md" />
+      </div>
+    );
+
+  /**
+   * Error State
+   */
+  if (error)
+    return (
+      <div className="rounded-md bg-red-50 p-4">
+        <div className="flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 text-red-800"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+            />
+          </svg>
+          <p className="font-medium text-red-800">
+            An Error Occured while generating the definition for{" "}
+            <span className="font-bold">{word}</span>.
+          </p>
+        </div>
       </div>
     );
 

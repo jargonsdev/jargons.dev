898234a3b56d4969006bd9519b65d9e7c5b730d2
Christine <shecoder30@gmail.com>
2025-10-01 21:28:01 +0000

    docs(fix): update jsdoc types
    Signed-off-by: Christine <shecoder30@gmail.com>

--- src/lib/hooks/use-word-editor.js
+++ src/lib/hooks/use-word-editor.js
@@ -3,7 +3,7 @@ import { $wordEditor } from "../stores/dictionary.js";
 
 /**
  * Custom hook serves as wrapper around `wordEditor` map store
- * @returns {{ title: string, content: string, setTitle: (title: string) => void, setContent: (content: string) => void }}
+ * * @returns {{ title: string, content: string, initialContent: string, setTitle: (title: string) => void, setContent: (content: string) => void, setInitialContent: (content: string) => void }}
  */
 export default function useWordEditor() {
   const word = useStore($wordEditor);

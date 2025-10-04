913b65d948ce2a0c8f5454af868c43fbb4ba8b70
babblebey <babblebey@gmail.com>
2025-10-04 22:26:54 +0100

    fix: update parameter import path and enhance success message in getNextStepPage function

--- dev/setup/lib/content.html.js
+++ dev/setup/lib/content.html.js
@@ -167,7 +167,7 @@ export function getStartPage(registerUrl, manifestJson) {
 
 /**
  * Generate the next step page HTML content
- * @param {import('../index').AppCredentials} appCredentials
+ * @param {import('./index').AppCredentials} appCredentials
  * @returns 
  */
 export function getNextStepPage(appCredentials) {
@@ -183,7 +183,7 @@ export function getNextStepPage(appCredentials) {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="height: 1.5rem; width: auto;">
                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
             </svg>
-            <span>GitHub App created</span>
+            <span>GitHub App \`${appCredentials.name}\` created</span>
         </div>
         <div style="margin-top: 10px; display: flex; align-items: center;">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="height: 1.5rem; width: auto;">
